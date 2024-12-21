798177ce906b7f9cf808837532a40ce2f98159dd
Marka <g.lkhagvadelger@gmail.com>
2024-12-21 16:56:35 +0800

    install confirmation

---
+++ pages/api/auth/InstallConfirmation.ts
@@ -0,0 +1,26 @@
+import { createHandler } from "@api/handler";
+import {
+  verifyUserByPhoneNumber
+} from "@lib/user/api/service";
+import { AppError } from "@util/errors";
+
+const handler = createHandler();
+
+handler.post(async (req, res) => {
+  try {
+    if (!req.body.phoneNumber)
+      throw AppError.BadRequest("validation.phoneNumber");
+    if (!req.body.confirmationCode) throw AppError.BadRequest("validation.pin");
+
+    const user = await verifyUserByPhoneNumber(
+      req.body.phoneNumber,
+      req.body.confirmationCode
+    );
+    if (!user) throw AppError.NotFound("user.not-found");
+
+    res.sendSuccess({ success: true });
+  } catch (e) {
+    res.sendError(e);
+  }
+});
+export default handler;
