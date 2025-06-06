c57b8e60ed4ba46dcfcd93af78da3a411951b197
bayar92 <50005309+bayar92@users.noreply.github.com>
2025-06-06 17:00:03 +0800

    Update test.tsx

--- pages/patient/test.tsx
+++ pages/patient/test.tsx
@@ -1,13 +1,32 @@
+// pages/patient/test.tsx
 import React from "react";
-const TestPage = () => {
-    
-    return (
-        <div>
-            <h1>Text Page</h1>
-            <p>API URL: {process.env.DENTAL_DB_USER}</p>
-            <p>API HOST: {process.env.DENTAL_DB_HOST}</p>
-            <p>API PORT: {process.env.DENTAL_DB_PORT}</p>
-        </div>
-    )
-}
+import { GetServerSideProps } from "next";
+
+type Props = {
+  dbUser: string;
+  dbHost: string;
+  dbPort: string;
+};
+
+const TestPage = ({ dbUser, dbHost, dbPort }: Props) => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>DB USER: {dbUser}</p>
+      <p>DB HOST: {dbHost}</p>
+      <p>DB PORT: {dbPort}</p>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      dbUser: process.env.DENTAL_DB_USER || "Not set",
+      dbHost: process.env.DENTAL_DB_HOST || "Not set",
+      dbPort: process.env.DENTAL_DB_PORT || "Not set",
+    },
+  };
+};
+
 export default TestPage;
